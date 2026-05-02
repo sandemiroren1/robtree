33fda6e868b69a0bcef71a8d817a08e9059f0b11
sandemiroren <sandemiroren1@gmail.com>
2026-05-03 01:07:40 +0200

    scheduler class

--- src/DataStructures/include/node_expansion_scheduler_abstract_class.h
+++ src/DataStructures/include/node_expansion_scheduler_abstract_class.h
@@ -2,12 +2,12 @@
 #define NODE_SCHEDULER
 #include "node.h"
 class Scheduler {
-  Scheduler(NodePtr tree);
 
 public:
-  virtual bool all_nodes_expanded();
-  virtual NodePtr get_next_node();
+  Scheduler(NodePtr tree);
+  virtual bool all_nodes_expanded() const;
+  virtual NodePtr get_next_node() const;
   virtual void set_expanded(NodeId node_id, bool expansion_status);
-  virtual bool get_expanded(NodeId node_id);
+  virtual bool get_expanded(NodeId node_id) const;
 };
 #endif
--- src/DataStructures/include/node_expansion_scheduler_simple.h
+++ src/DataStructures/include/node_expansion_scheduler_simple.h
@@ -2,10 +2,21 @@
 #define SIMPLE_NODE_SCHEDULER
 #include "node.h"
 #include "node_expansion_scheduler_abstract_class.h"
+#include <set>
+using NodePtrList = std::vector<NodePtr>;
+using ExpandedNodes = std::set<NodeId>;
 class SimpleScheduler : public Scheduler {
+public:
   SimpleScheduler(NodePtr tree);
-  bool all_nodes_expanded();
-  NodePtr get_next_node();
+  bool all_nodes_expanded() const;
+  NodePtr get_next_node() const;
   void set_expanded(NodeId node_id, bool expansion_status);
+  bool get_expanded(NodeId node_id) const;
+
+private:
+  NodePtrList schedule;
+  ExpandedNodes expanded_nodes;
+  NodePtrList::size_type index_of_next_node_in_schedule;
+  void dfs(NodePtr tree, NodePtrList &schedule);
 };
 #endif
